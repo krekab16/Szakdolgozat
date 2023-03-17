2919d72ef39ab1055c7ed6989f0c5d32fbe0c10e
krekab16 <119123770+krekab16@users.noreply.github.com>
2023-03-17 18:17:23 +0100

    Log in screen business logic (#7)
    * Bejelentkezes kepernyo business logic elkeszitese

--- application/lib/viewmodel/log_in_screen_view_model.dart
+++ application/lib/viewmodel/log_in_screen_view_model.dart
@@ -1,26 +1,65 @@
 import 'package:flutter/material.dart';
+import '../model/user_model.dart';
+import '../service/user_database_service.dart';
 import '../utils/route_constants.dart';
 import '../utils/text_strings.dart';
 
 class LogInViewModel with ChangeNotifier {
+  final UserModel _user = UserModel(
+      name: '', username: '', email: '', password: '', isOrganizer: false);
+  final UserDatabaseService service = UserDatabaseService();
+  List<String> errorMessages = [];
+
+  String getEmail() {
+    return _user.email;
+  }
+
+  String getPassword() {
+    return _user.password;
+  }
+
+  void setEmail(String email) {
+    _user.email = email;
+    notifyListeners();
+  }
+
+  void setPassword(String password) {
+    _user.password = password;
+    notifyListeners();
+  }
+
+  Future<void> login() async {
+    try {
+      await service.logInUser(_user.toDTO());
+      errorMessages = [];
+    } catch (e) {
+      if (e.toString().isNotEmpty) {
+        errorMessages = [e.toString()];
+      } else {
+        errorMessages = [standardErrorMessage];
+      }
+    }
+    notifyListeners();
+  }
+
   void navigateToHome(BuildContext context) {
     Navigator.pushReplacementNamed(context, homeRoute);
   }
 
   String? validateEmail(String value) {
     if (value.isEmpty) {
-      return mustEnterEmail;
+      return mustEnterEmailErrorMessage;
     } else if (!value.contains("@")) {
-      return wrong_email;
+      return wrongEmailErrorMessage;
     }
     return null;
   }
 
   String? validatePassword(String value) {
     if (value.isEmpty) {
-      return mustEnterPassword;
+      return mustEnterPasswordErrorMessage;
     } else if (value.length < 6) {
-      return validate_password;
+      return validatePasswordErrorMessage;
     }
     return null;
   }
