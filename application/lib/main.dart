060c9b7818c370e4a900773b0384c3d87be912f7
Kun Réka Bianka <kunreka2001@gmail.com>
2023-04-21 19:40:05 +0200

    Platform opciok

--- application/lib/main.dart
+++ application/lib/main.dart
@@ -21,11 +21,15 @@ import 'package:application/viewmodel/sign_up_screen_view_model.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'firebase_options.dart';
 import 'model/user_model.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
--- application/windows/flutter/generated_plugin_registrant.cc
+++ application/windows/flutter/generated_plugin_registrant.cc
@@ -7,11 +7,17 @@
 #include "generated_plugin_registrant.h"
 
 #include <geolocator_windows/geolocator_windows.h>
+<<<<<<< HEAD
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
+=======
+>>>>>>> 4ba582e (Platform opciok)
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   GeolocatorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("GeolocatorWindows"));
+<<<<<<< HEAD
   PermissionHandlerWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
+=======
+>>>>>>> 4ba582e (Platform opciok)
 }
