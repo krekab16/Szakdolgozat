7f65b6990b23b8712a702a147858157af8dba758
Kun Réka Bianka <kunreka2001@gmail.com>
2023-04-22 21:37:52 +0200

    Terkep elkeszitese

--- application/lib/ui/map_screen_ui.dart
+++ application/lib/ui/map_screen_ui.dart
@@ -38,7 +38,7 @@ class _MapScreenState extends State<MapScreen> {
               myLocationButtonEnabled: true,
               markers: mapViewModel.getCurrentMarker(),
             ),
-          ]
+          ],
         ],
       ),
     );
