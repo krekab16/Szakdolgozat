b05b07ceea671838daef1a7e860e7d8a64b00402
krekab16 <119123770+krekab16@users.noreply.github.com>
2023-04-26 20:37:26 +0200

    Kedvencek kepernyo ui elkeszitese (#14)
    Kedvencek kepernyo ui elkeszitese

---
+++ application/lib/viewmodel/favourite_event_screen_view_model.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import '../model/favourite_event_model.dart';
+
+class FavouriteEventViewModel with ChangeNotifier {
+  FavouriteEventModel favouriteEventModel = FavouriteEventModel();
+}
