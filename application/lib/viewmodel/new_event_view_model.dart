509b563d7832ceda6401ada3022db0a6d5b0f6ec
Kun Réka Bianka <kunreka2001@gmail.com>
2023-04-27 21:28:14 +0200

    Nem hasznalt fuggvenyek eltavolitasa

--- application/lib/viewmodel/new_event_view_model.dart
+++ application/lib/viewmodel/new_event_view_model.dart
@@ -1,8 +1,6 @@
-import 'package:application/model/event_model.dart';
 import 'package:flutter/material.dart';
 
 class NewEventScreenViewModel with ChangeNotifier {
-  final EventModel _event = EventModel.createEmpty();
 
   final List<String> categories = [
     'Sport',
@@ -15,40 +13,4 @@ class NewEventScreenViewModel with ChangeNotifier {
     'Politika',
     'Kultúra'
   ];
-
-
-  void setName(String name) {
-    _event.name = name;
-    notifyListeners();
-  }
-
-  void setAdress(String address) {
-    _event.address = address;
-    notifyListeners();
-  }
-
-  void setCategory(String category) {
-    _event.category = category;
-    notifyListeners();
-  }
-
-  void setDate(DateTime date) {
-    _event.date = date;
-    notifyListeners();
-  }
-
-  void setStuffLimit(int stuffLimit) {
-    _event.stuffLimit = stuffLimit;
-    notifyListeners();
-  }
-
-  void setImage(String image) {
-    _event.image = image;
-    notifyListeners();
-  }
-
-  void setDescription(String description) {
-    _event.description = description;
-    notifyListeners();
-  }
 }
